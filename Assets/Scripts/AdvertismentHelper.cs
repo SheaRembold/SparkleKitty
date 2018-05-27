1f241c1323ac2a9b22d619a4fde3d49f794f5ca8
sirerr <sirerr@gmail.com>
2018-05-26 21:54:26 -0400

    made some changes in advertiser stuff

---
+++ Assets/Scripts/AdvertismentHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdvertismentHelper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            ScreenCapture.CaptureScreenshot("C:/Users/POG-LW/Documents/cats.png",2);
+        }
+	}
+
+  
+}
